2608b65c172ff2619031af8fd478c8f5adb567dc
Elena Ursu <elena.ursu@cegeka.com>
2020-07-14 13:38:35 +0300

    first commit - db created at runtime

---
+++ Core.Domain/Migrations/20200701113520_RunSqlScript.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.IO;
+
+namespace Core.Domain.Migrations
+{
+    public partial class RunSqlScript : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string sqlText = File.ReadAllText(@"DataSeed.txt");
+            migrationBuilder.Sql(sqlText);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
